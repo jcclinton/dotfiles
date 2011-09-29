f83556aa31c0a842a1c855eaebfe55ed72e824a2
Jamie Clinton <jclinton@osx.local>
2011-09-29 01:35:27 -0700

    adding my own changes to osx defaults

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 git pull
-read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+#read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+#if [[ $REPLY =~ ^[Yy]$ ]]; then
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-fi
+#fi
 source "$HOME/.bash_profile"
